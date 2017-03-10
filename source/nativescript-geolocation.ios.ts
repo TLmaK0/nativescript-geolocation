41dc4ee06c643b559eff5ddd79f387b996df5b45
Hugo Freire <user904184@77-240-12-19.rdns.melbourne.co.uk>
2017-03-10 13:41:18 +0000

    Allows background location updates in ios 9

--- source/nativescript-geolocation.ios.ts
+++ source/nativescript-geolocation.ios.ts
@@ -322,6 +322,7 @@ export class LocationMonitor implements LocationMonitorDef {
         iosLocManager.distanceFilter = options ? options.updateDistance : minRangeUpdate;
         locationManagers[locListener.id] = iosLocManager;
         locationListeners[locListener.id] = locListener;
+        iosLocManager.allowsBackgroundLocationUpdates = true;
         return iosLocManager;
     }
 }
